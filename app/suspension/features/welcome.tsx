3000581f0270b87d61b1c0e33af61f88d1a844f7
chenyueban <jasonchan0527@gmail.com>
2020-11-03 17:27:56 +0800

    feat: 悬浮框 welcome 页面加入 slogan

--- app/suspension/features/welcome.tsx
+++ app/suspension/features/welcome.tsx
@@ -3,12 +3,21 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 
 import Layout from '../components/Layout';
+import Text from '../components/Text';
 import routes from '../constants/routes.json';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     height: '100%',
     fontSize: 40,
+  },
+  slogan: {
+    fontSize: 28,
+    marginTop: theme.spacing(4),
+  },
+  slogan2: {
+    fontSize: 24,
+    marginTop: theme.spacing(1),
   },
 }));
 
@@ -17,8 +26,14 @@ const Welcome: React.FC = () => {
 
   return (
     <Layout className={classes.root}>
-      <Link to={routes.HEROSELECTION}>to heroes</Link>
-      <Link to={routes.GAMEOVER}>to gameover</Link>
+      {process.env.NODE_ENV === 'development' && (
+        <>
+          <Link to={routes.HEROSELECTION}>to heroes</Link>
+          <Link to={routes.GAMEOVER}>to gameover</Link>
+        </>
+      )}
+      <Text className={classes.slogan}>欢迎使用</Text>
+      <Text className={classes.slogan2}>酒馆战棋统计工具</Text>
     </Layout>
   );
 };
