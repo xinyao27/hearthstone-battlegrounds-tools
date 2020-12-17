2ac1735d214a45d24811de66491817b6ee320cad
chenyueban <jasonchan0527@gmail.com>
2020-12-17 11:26:33 +0800

    feat(suspension): 战棋图鉴页优化图鉴展示形式

--- app/renderer/suspension/pages/handbook.tsx
+++ app/renderer/suspension/pages/handbook.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 
 import Layout from '@suspension/components/Layout';
 import SwitchBattleAndHandbook from '@suspension/components/SwitchBattleAndHandbook';
@@ -19,19 +20,19 @@ function groupByTier(list: typeof minions) {
     };
   }, {});
 }
-function getTierCN(tier: string) {
+function getTierCN(tier: number) {
   switch (tier) {
-    case '1':
+    case 1:
       return '一';
-    case '2':
+    case 2:
       return '二';
-    case '3':
+    case 3:
       return '三';
-    case '4':
+    case 4:
       return '四';
-    case '5':
+    case 5:
       return '五';
-    case '6':
+    case 6:
       return '六';
     default:
       return '一';
@@ -42,19 +43,25 @@ const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
   },
+  switch: {},
+  switchItem: {
+    display: 'inline-block',
+    width: 34,
+    filter: 'grayscale(1)',
+    transition: `all ${theme.transitions.easing.sharp} ${theme.transitions.duration.shortest}ms`,
+    '&:hover': {
+      filter: 'grayscale(0.2) drop-shadow(0px 0px 2px #f9cd0d)',
+    },
+  },
+  switchItemActive: {
+    filter: 'grayscale(0) drop-shadow(0px 0px 6px #f9cd0d)',
+  },
   group: {},
   title: {
-    display: 'flex',
-    alignItems: 'center',
+    fontSize: 18,
+    textAlign: 'center',
+    marginTop: theme.spacing(1),
     marginBottom: theme.spacing(2),
-    '& > img': {
-      display: 'inline-block',
-      width: 40,
-    },
-    '& > p': {
-      display: 'inline-block',
-      marginLeft: theme.spacing(1),
-    },
   },
   minions: {},
 }));
@@ -62,32 +69,42 @@ const useStyles = makeStyles((theme) => ({
 const Handbook: React.FC = () => {
   const classes = useStyles();
   const data = groupByTier(minions);
+  const [currentTier, setCurrentTier] = React.useState<number>(1);
 
   return (
     <Layout className={classes.root}>
       <SwitchBattleAndHandbook current="handbook" />
 
-      {Object.keys(data)?.map((tier) => (
-        <div className={classes.group} key={tier}>
-          <div className={classes.title}>
-            <img
-              src={require(`@shared/assets/images/tier-${tier}.png`).default}
-              alt="tier"
-            />
-            <Text>{`${getTierCN(tier)}星酒馆`}</Text>
-          </div>
+      <div className={classes.switch}>
+        {[1, 2, 3, 4, 5, 6].map((tier) => (
+          // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
+          <img
+            className={clsx(classes.switchItem, {
+              [classes.switchItemActive]: tier === currentTier,
+            })}
+            src={require(`@shared/assets/images/tier-${tier}.png`).default}
+            alt="tier"
+            onClick={() => setCurrentTier(tier)}
+            key={tier}
+          />
+        ))}
+      </div>
 
-          <div className={classes.minions}>
-            {data?.[(tier as unknown) as number]?.map((minion) => (
-              <MinionCard
-                minionName={minion.name}
-                props={{ TECH_LEVEL: tier }}
-                key={minion.id}
-              />
-            ))}
-          </div>
+      <div className={classes.group}>
+        <div className={classes.title}>
+          <Text>{`${getTierCN(currentTier)}星酒馆`}</Text>
         </div>
-      ))}
+
+        <div className={classes.minions}>
+          {data?.[currentTier]?.map((minion) => (
+            <MinionCard
+              minionName={minion.name}
+              props={{ TECH_LEVEL: currentTier.toString() }}
+              key={minion.id}
+            />
+          ))}
+        </div>
+      </div>
     </Layout>
   );
 };
