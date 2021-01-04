54b1b9dac24e72a5903faed36a0142d0135734d9
chenyueban <jasonchan0527@gmail.com>
2021-01-04 12:02:19 +0800

    feat(suspension): 没有查询到可选英雄数据时给予提示

--- src/renderer/suspension/pages/heroSelection.tsx
+++ src/renderer/suspension/pages/heroSelection.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Layout from '@suspension/components/Layout';
 import HeroCard from '@suspension/components/HeroCard';
+import Text from '@suspension/components/Text';
 import useStateFlow from '@suspension/hooks/useStateFlow';
 import useHeroes from '@shared/hooks/useHeroes';
 
@@ -11,9 +12,13 @@ const HeroSelection: React.FC = () => {
 
   return (
     <Layout>
-      {stateFlow?.HERO_TOBE_CHOSEN?.result?.map((name: string) => (
-        <HeroCard heroId={getHeroId(name)} key={name} />
-      ))}
+      {stateFlow?.HERO_TOBE_CHOSEN?.result?.length ? (
+        stateFlow.HERO_TOBE_CHOSEN.result.map((name: string) => (
+          <HeroCard heroId={getHeroId(name)} key={name} />
+        ))
+      ) : (
+        <Text>抱歉没有查询到可选英雄的数据，也许下一盘就好啦</Text>
+      )}
     </Layout>
   );
 };
