c00cec151022e597425d088abd49093d88c6d017
chenyueban <jasonchan0527@gmail.com>
2021-01-23 21:50:46 +0800

    fix(main): 修复 mac 系统下 command+Q 组合键在窗口未激活状态下也失效的问题

--- src/shared/api/request.ts
+++ src/shared/api/request.ts
@@ -4,7 +4,8 @@ import qs from 'querystring';
 export default axios.create({
   baseURL:
     process.env.NODE_ENV === 'development'
-      ? 'http://localhost:23333/api'
+      ? // ? 'http://localhost:23333/api'
+        'https://hs.chenyueban.com/api'
       : 'https://hs.chenyueban.com/api',
   timeout: process.env.NODE_ENV === 'development' ? 10000 : 15000,
   paramsSerializer: (params) => qs.stringify(params),
