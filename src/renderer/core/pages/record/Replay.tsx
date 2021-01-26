eb18615a7cc525770028c96ed45c430f97fdeec9
chenyueban <jasonchan0527@gmail.com>
2021-01-26 11:30:50 +0800

    fix(core): 修复复盘中没有随从导致阵容区域塌陷的问题

--- src/renderer/core/pages/record/Replay.tsx
+++ src/renderer/core/pages/record/Replay.tsx
@@ -40,6 +40,7 @@ const useStyles = makeStyles((theme) =>
       cursor: 'pointer',
     },
     lineup: {
+      minHeight: 120,
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
